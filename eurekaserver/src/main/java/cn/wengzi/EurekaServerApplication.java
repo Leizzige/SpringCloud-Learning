7dfddb5d6e46e71cd428884435a65a926fb8e11f
wengzi <Hush.TianLLei@outlook.com>
2019-10-18 18:01:40 +0800

    注册中心

---
+++ eurekaserver/src/main/java/cn/wengzi/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package cn.wengzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wengzi
+ * @date 2019/10/18 下午15:32
+ * @Description 注册中心
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
