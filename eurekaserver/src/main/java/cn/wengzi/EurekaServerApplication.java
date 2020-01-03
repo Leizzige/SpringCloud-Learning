4fbbfe6ffaa51b628bc58b1b774bc4a4ab78b66c
tianlei <lei.tl@bitsun-inc.com>
2020-01-03 22:56:46 +0800

    eureka注册中心

--- eurekaserver/src/main/java/cn/wengzi/EurekaServerApplication.java
+++ eurekaserver/src/main/java/cn/wengzi/EurekaServerApplication.java
@@ -2,12 +2,13 @@ package cn.wengzi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * @author wengzi
+ * @author leizzige
  * @date 2019/10/18 afternoon 15:32
- * @Description 注册中心
+ * desc: 注册中心
  */
 @SpringBootApplication
 @EnableEurekaServer
